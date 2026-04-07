dd7eb00cca4d6ffd907428bb4cb448b8f84ac732
Peter Olsen <olse0321@umn.edu>
2026-04-06 23:17:14 -0500

    add first basic test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class ApiTest {
+    @Test
+    void basicAPITest() {
+        given()
+            .baseUri("http://localhost:8080")
+        .when()
+            .get("/returnMyNumber?number=5")
+        .then()
+            .statusCode(200)
+            .body("number", equalTo("5"));
+    }
 }
